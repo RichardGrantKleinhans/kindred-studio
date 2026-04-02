af06e1673e43410824a5f3a1b6e223ea6b89fe31
RichardGrantKleinhans <ratatoskr.solutions@proton.me>
2026-04-02 01:56:19 -0400

    Update main.jsx
    iOS rendered with curly quotes....yes this was done from iOS in mobile safari...

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from ‘react’
+import React from 'react'
 import ReactDOM from ‘react-dom/client’
-import KindredStudio from ‘./kindred-studio’
+import KindredStudio from './kindred-studio'
 
 ReactDOM.createRoot(document.getElementById(‘root’)).render(
 <React.StrictMode>
