bdec48f9c459aeab252a9bf84c13e065b1bdf4b1
RichardGrantKleinhans <ratatoskr.solutions@proton.me>
2026-04-02 00:20:56 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from ‘react’
+import ReactDOM from ‘react-dom/client’
+import KindredStudio from ‘./kindred-studio’
+
+ReactDOM.createRoot(document.getElementById(‘root’)).render(
+<React.StrictMode>
+<KindredStudio />
+</React.StrictMode>
+)
